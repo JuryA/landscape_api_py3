ac310978fe1ac18c16ff9c4a305b8fcc97f60424
Altman <jiri.altman@konicaminolta.cz>
2020-06-10 03:13:19 +0200

    build: update dependency files

--- docs/conf.py
+++ docs/conf.py
@@ -22,6 +22,9 @@ import sys
 
 import landscape_api_py3
 
+# import rtcat_sphinx_theme
+
+
 sys.path.insert(0, os.path.abspath(".."))
 
 
@@ -34,6 +37,12 @@ sys.path.insert(0, os.path.abspath(".."))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ["sphinx.ext.autodoc", "sphinx_epytext", "sphinx.ext.viewcode"]
+
+autodoc_default_options = {
+    "members": True,
+    "inherited-members": True,
+    "show-inheritance": True,
+}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -85,7 +94,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
+# html_theme = "alabaster"
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
@@ -98,6 +107,8 @@ html_theme = "alabaster"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
+# html_theme = "rtcat_sphinx_theme"
+# html_theme_path = [rtcat_sphinx_theme.get_html_theme_path()]
 
 # -- Options for HTMLHelp output ---------------------------------------
 
