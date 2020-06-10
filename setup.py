6107204afe3ee396f89c6def302da4a3e97e91c1
Altman <jiri.altman@konicaminolta.cz>
2020-06-11 01:48:37 +0200

    Bump version: 0.4.2 → 0.5.0

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/jurya/landscape_api_py3",
-    version="0.4.2",
+    version="0.5.0",
     zip_safe=False,
     entry_points={"console_scripts": ["landscape-api=landscape_api.__main__:cli"]},
     package_data={"landscape_api": ["schemas.json"]},
