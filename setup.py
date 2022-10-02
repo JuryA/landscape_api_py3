be2a3bc60e09cf76e22ded5c2e4c9297744a0fda
Jiří Altman <altman.jiri@gmail.com>
2022-10-02 04:47:39 +0200

    Bump version: 0.8.0 → 0.9.0

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/jurya/landscape_api_py3",
-    version="0.8.0",
+    version="0.9.0",
     zip_safe=False,
     entry_points={"console_scripts": ["landscape-api=landscape_api.__main__:cli"]},
     package_data={"landscape_api": ["schemas.json"]},
